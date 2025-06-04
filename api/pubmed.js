477759a425dc8b87d6f911bbd957a4d1d0bcb7a1
Romylson <romylson98@gmail.com>
2025-06-04 11:13:00 -0300

    Resolver api de ensaios clinicos e pubmed summary,pubmed

--- api/pubmed.js
+++ api/pubmed.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
   const q = req.query.q || "alzheimer";
-  const NCBI_API_KEY = "5ee3209efa43505d1837bf665243eaf15308";
-  const url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=${encodeURIComponent(q)}&retmax=3&sort=pub+date&retmode=json&api_key=${NCBI_API_KEY}`;
+  const apiKey = "5ee3209efa43505d1837bf665243eaf15308";
+
+  const url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=${encodeURIComponent(q)}&retmax=3&sort=pub+date&retmode=json&api_key=${apiKey}`;
 
   try {
     const response = await fetch(url);
