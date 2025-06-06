4392b8e3bed95e81749ff5de0d2dc49d4072c759
Romylson <romylson98gmail.com>
2025-06-05 23:45:29 -0300

    Inclusão de jogos e artigos e erros

--- src/components/Artigos/ArtigosCientificosTabs.jsx
+++ src/components/Artigos/ArtigosCientificosTabs.jsx
@@ -1,7 +1,7 @@
 // ArtigosCientificosTabs.jsx (com filtro por tipo de estudo)
 import React, { useState } from "react";
 import { Tabs, Tab, ButtonGroup, ToggleButton } from "react-bootstrap";
-import ArtigosPubmed from "./ArtigosPubmed.jsx";
+import ArtigosPubmed from "./ArtigosPubmed";
 
 
 const tiposEstudo = [
