8b1724066f7739b2c9e3723c5aef371cbc2f80d5
Romylson <romylson98gmail.com>
2025-07-09 23:46:10 -0300

    Ajustando responsividade no celular de tela 5

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,3 +1,4 @@
+// src/components/AppHeader.jsx
 import React from "react";
 import logoNppmPpgf from "../assets/logo-nppm-ppgf.jpeg";
 import logoLafac from "../assets/logo-lafac-ufpi.jpeg";
@@ -5,10 +6,18 @@ import logoLafac from "../assets/logo-lafac-ufpi.jpeg";
 export default function AppHeader() {
   return (
     <header className="app-header">
-      <div className="header-logos">
-        <img src={logoNppmPpgf} alt="NPPM PPGF" />
+      <div className="app-header-inner">
+        <img
+          src={logoNppmPpgf}
+          alt="NPPM PPGF"
+          className="header-logo"
+        />
         <div className="divider-line" />
-        <img src={logoLafac} alt="LAFAC-UFPI" />
+        <img
+          src={logoLafac}
+          alt="LAFAC-UFPI"
+          className="header-logo"
+        />
         <div className="site-title">Plantamente</div>
       </div>
     </header>
