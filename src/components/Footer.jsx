69a0c3ce9b5f65a758228e1b2884f35a6f712006
Romylson <romylson98gmail.com>
2025-09-13 15:24:32 -0300

    Inserindo endereço UFPI ajustes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import LogoInstitucional from "../components/LogoInstitucional";
 export default function Footer() {
   return (
     <footer className="w-full bg-green-200 py-2 border-t border-green-500 mt-4 flex flex-col items-center">
-      <LogoInstitucional direction="row" small />
+      <LogoInstitucional direction="row" small showAddress />
       <div className="text-xs text-black mt-1 text-center">
         &copy; {new Date().getFullYear()} Plantamente • UFPI • Todos os direitos reservados
       </div>
--- src/components/LogoInstitucional.jsx
+++ src/components/LogoInstitucional.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import logoNppmPpgf from "../assets/logo-nppm-ppgf.jpeg";
 import logoLafac from "../assets/logo-lafac-ufpi.jpeg";
 
-export default function LogoInstitucional({ direction = "row", small = false }) {
+export default function LogoInstitucional({
+  direction = "row",
+  small = false,
+  showAddress = false,
+}) {
   return (
     <div
+      className="logo-inst flex flex-col justify-center items-center"
       style={{ width: "100%" }}
     >
-     <div
+      <div
         className={`flex ${direction === "row" ? "flex-row" : "flex-col"} justify-center items-center gap-4`}
       >
         <img
@@ -33,9 +38,11 @@ export default function LogoInstitucional({ direction = "row", small = false }) 
           }}
         />
       </div>
+      {showAddress && (
       <div className="text-xs text-center mt-2">
         UFPI – Campus Universitário Ministro Petrônio Portella, Ininga, Teresina‑PI, 64049‑550
       </div>
+       )}
     </div>
   );
 }
