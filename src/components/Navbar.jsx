891dfc67bd8657dad9f5123fd95e35545a517798
Romylson <romylson98gmail.com>
2025-09-14 01:54:13 -0300

    Inserindo plantas e moleculas movendo ajustes cor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,6 +43,7 @@ export default function Navbar() {
             >
               🎨
             </button>
+            </div>
             {/* Dropdown de idiomas */}
             <div className="dropdown">
               <button
