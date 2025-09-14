6c86f531a331674adfe2e83ec983647549cdcef2
Romylson <romylson98gmail.com>
2025-09-14 02:15:39 -0300

    Inserindo historico e apresentacao 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import LogoInstitucional from "../components/LogoInstitucional";
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-green-200 py-2 border-t border-green-500 mt-4 flex flex-col items-center">
+     <footer className="w-full bg-secondary text-surface py-2 border-t border-primary mt-4 flex flex-col items-center">
       <LogoInstitucional direction="row" small showAddress />
       <div className="text-xs text-black mt-1 text-center">
         &copy; {new Date().getFullYear()} Plantamente • UFPI • Todos os direitos reservados
