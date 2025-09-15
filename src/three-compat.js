889fa2c2051c4b5db57978e97885fe014ba34ae0
Romylson <romylson98gmail.com>
2025-09-14 23:08:05 -0300

    Inserir paleta de cores ajustes

---
+++ src/three-compat.js
@@ -0,0 +1,4 @@
+import * as Three from '../node_modules/three/src/Three.js';
+export * from '../node_modules/three/src/Three.js';
+export default Three;
+export class BatchedMesh extends Three.Mesh {}
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      three: path.resolve(__dirname, 'src/three-compat.js'),
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3001'
