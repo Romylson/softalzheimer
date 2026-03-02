43ffd5574dc8df08bd8f863a393f502bf822fb36
Romylson <romylson98@gmail.com>
2026-03-02 14:56:44 -0300

    Ajusta carrossel da home para layout estilo Seduc-PI

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,33 +1,39 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import "./ImageCarousel.css";
+
+const slides = [
+  {
+    src: "/images/alzheimer.png",
+    alt: "Alzheimer",
+    titulo: "Informação científica com linguagem acessível",
+  },
+  {
+    src: "/images/fisiopatologia.png",
+    alt: "Fisiopatologia",
+    titulo: "Compreenda mecanismos da doença em visual didático",
+  },
+  {
+    src: "/images/plantas.jpg",
+    alt: "Plantas medicinais",
+    titulo: "Evidências sobre plantas e compostos naturais",
+  },
+];
 
 export default function ImageCarousel() {
   return (
-    <Carousel className="mb-4" interval={3000}>
-      <Carousel.Item>
-        <img
-          className="d-block w-75 mx-auto rounded"
-          style={{ maxHeight: "400px", objectFit: "cover" }}
-          src="/images/alzheimer.png"
-          alt="Alzheimer"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-75 mx-auto rounded"
-          style={{ maxHeight: "400px", objectFit: "cover" }}
-          src="/images/plantas.jpg"
-          alt="Plantas medicinais"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-75 mx-auto rounded"
-          style={{ maxHeight: "400px", objectFit: "cover" }}
-          src="/images/farmaco.jpeg"
-          alt="Fármacos"
-        />
-      </Carousel.Item>
+    <Carousel className="mb-4 seduc-like-carousel" interval={3500}>
+      {slides.map((slide) => (
+        <Carousel.Item key={slide.alt}>
+          <div className="carousel-slide-wrap">
+            <img className="carousel-banner-img" src={slide.src} alt={slide.alt} />
+            <div className="carousel-overlay">
+              <span className="carousel-badge">PlantaMente</span>
+              <h3>{slide.titulo}</h3>
+            </div>
+          </div>
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 }
