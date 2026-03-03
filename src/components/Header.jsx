0eec91d0fe5a5bd22f3e7327b2bcab063187809e
Romylson <romylson98@gmail.com>
2026-03-03 20:13:30 -0300

    ajustar cor do header novamente

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,10 @@ import LogoInstitucional from "../components/LogoInstitucional";
 
 export default function Header() {
   return (
-    <header className="w-full py-2 border-b mb-2 flex justify-center"
-        style={{ backgroundColor: "var(--header-bg)" }}>
+    <header
+  className="w-full py-2 border-b mb-2 flex justify-center"
+  style={{ backgroundColor: "var(--background-color)" }}
+    >
       <LogoInstitucional direction="row" />
     </header>
   );
