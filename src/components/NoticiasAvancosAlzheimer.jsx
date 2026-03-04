8548cd013b45268b336003f6b91eff91be3b2341
Romylson <romylson98@gmail.com>
2026-03-04 17:48:06 -0300

    testando link

--- src/components/NoticiasAvancosAlzheimer.jsx
+++ src/components/NoticiasAvancosAlzheimer.jsx
@@ -10,7 +10,8 @@ const ultimasNoticias = [
       "Novos estudos reforçam o uso combinado de p-tau e NfL para identificar risco de declínio cognitivo em fases iniciais.",
     data: "10 de janeiro de 2026",
     imagem: "/images/biomarcadores-noticia.jpg",
-    link: "https://alz-journals.onlinelibrary.wiley.com/",
+    link: "/noticias/biomarcadores-sanguineos-triagem-precoce",
+
   },
   {
     titulo: "Sono de má qualidade é associado à pior progressão cognitiva",
